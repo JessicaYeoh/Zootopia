01f245ff1036bafd5b0a79058a771b7694da75fe
JessicaYeoh <30559012+JessicaYeoh@users.noreply.github.com>
2018-03-26 09:21:48 +1000

    fixed update user profile function processing

--- controller/update_profile.php
+++ controller/update_profile.php
@@ -4,21 +4,11 @@ header('Content-Type: application/json');
 include '../model/db.php';
 $conn = connect();
 
+$login_ID = $_GET['loginID'];
 
-// if(isset($_GET['loginID'])) {
-//   if($_GET['loginID'] == $_SESSION['userID']) {
-//     $result = getOneUser($_SESSION['userID']);
-//     if(is_array($result)) {
-//       echo json_encode($result);
-//     } else {
-//       echo json_encode(Array('userdata'=>false));
-//     }
-//   }
-// }
-
-if(isset($_GET['loginID'])) {
+if(isset($login_ID)) {
   // if($_GET['loginID'] == $_SESSION['userID']) {
-    $result = getOneUser($_GET['loginID']);
+    $result = getOneUser($login_ID);
     if(is_array($result)) {
       echo json_encode($result);
     } else {
@@ -28,10 +18,13 @@ if(isset($_GET['loginID'])) {
 }
 
 if(isset($_POST)) {
-  if(updateUser($_POST, $_GET['loginID'])) {
-    echo json_encode(Array('userUpdate'=>true));
+
+  if(updateUser($_POST, $login_ID)) {
+   //  echo json_encode(Array('userUpdate'=>true));
+      return true;
   } else {
-    echo json_encode(Array('userUpdate'=>false));
+   //  echo json_encode(Array('userUpdate'=>false));
+      return false;
   }
   exit();
 }
