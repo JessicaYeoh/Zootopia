843f7ccdb97b91e0676b9eeeb07ca826c0ee0ed2
JessicaYeoh <30559012+JessicaYeoh@users.noreply.github.com>
2018-03-26 09:28:23 +1000

    updated with try catch, debugged update query and select query

--- model/db.php
+++ model/db.php
@@ -1,13 +1,37 @@
 <?php
 function connect(){
-$conn = new PDO("mysql:host=localhost;dbname=zootopia", 'root', '');
-// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  try {
+      $conn = new PDO("mysql:host=localhost;dbname=zootopia", 'root', '');
+
+      // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+      }
+  catch(PDOException $e) //error mode
+    {
+    $error_message = $e->getMessage();
+?>
+
+<h1>Database Connection Error</h1>
+<p>There was an error connecting to the database.</p>
+<!-- display the error message -->
+<p>Error message:
+  <?php
+    echo $error_message;
+  ?>
+</p>
+
+<?php
+  die();
+  }
+
 return $conn;
 }
+?>
 
+<?php
 function getOneUser($userID){
   $conn = connect();
-  $getOneUser = "SELECT * FROM tbllogin inner join tbluser on tbllogin.loginID = tbluser.loginID WHERE loginID = :lid;";
+  $getOneUser = "SELECT * FROM tbllogin inner join tbluser on tbllogin.loginID = tbluser.loginID WHERE tbllogin.loginID = :lid;";
   $stmt = $conn->prepare($getOneUser);
   $stmt->bindParam(':lid', $userID, PDO::PARAM_INT);
   $stmt->execute();
@@ -16,18 +40,23 @@ function getOneUser($userID){
 
 function updateUser($postdata, $userID) {
   $conn = connect();
-  $contentquery = "UPDATE tbllogin inner join tbluser on tbllogin.loginID = tbluser.loginID SET firstName = :fname, lastName = :surname, loginUsername = :email WHERE loginID = :lid;";
+  $contentquery = "UPDATE tbllogin inner join tbluser on tbllogin.loginID = tbluser.loginID SET tbluser.firstName = :fname, tbluser.lastName = :surname, tbllogin.loginUsername = :email WHERE tbllogin.loginID = :lid;";
   $stmt = $conn->prepare($contentquery);
-  $stmt->bindParam(':fname', test_user_input($postdata['profile_first_name']), PDO::PARAM_STR);
-  $stmt->bindParam(':surname', test_user_input($postdata['profile_last_name']), PDO::PARAM_STR);
-  $stmt->bindParam(':email', test_user_input($postdata['profile_email']), PDO::PARAM_STR);
+  $stmt->bindParam(':fname', test_user_input($postdata['fname']), PDO::PARAM_STR);
+  $stmt->bindParam(':surname', test_user_input($postdata['lname']), PDO::PARAM_STR);
+  $stmt->bindParam(':email', test_user_input($postdata['email']), PDO::PARAM_STR);
   $stmt->bindParam(':lid', test_user_input($userID), PDO::PARAM_INT);
   $stmt->execute();
-  if($stmt->rowCount() > 0) {
+  //create logic here
+  /* if($stmt->rowCount() > 0) {
     return true;
   } else {
     return false;
+  } */
+  if($postdata['fname'] ==null ||$postdata['lname']==null || $postdata['email']==null || $userID ==null){
+      return false;
   }
+  return true;
 }
 
 function test_user_input($data) {
