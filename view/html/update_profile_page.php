1704ef97f1a1e5f44a5a0d974facb38d795053ae
JessicaYeoh <30559012+JessicaYeoh@users.noreply.github.com>
2018-03-27 12:17:45 +1000

    AJAX update debugged

--- view/html/update_profile_page.php
+++ view/html/update_profile_page.php
@@ -19,7 +19,7 @@ $updateProfile = getOneUser($login_ID);
 
 $firstName = $updateProfile['firstName'];
 $surname = $updateProfile['lastName'];
-$address = $updateProfile['addressID'];
+// $address = $updateProfile['addressID'];
 $email = $updateProfile['loginUsername'];
 ?>
 
@@ -52,27 +52,48 @@ $email = $updateProfile['loginUsername'];
               <input type="text" class="form-control" id="profile_last_name" name="lname" value="<?php echo $surname; ?>">
             </div>
 
-            <!-- <div class="form-group">
+            <div class="form-group">
               <label for="address">Address</label>
               <input id="address" type="text" class="form-control" name="address">
             </div>
 
             <div class="form-group">
-              <label for="profile_location">Location</label>
-              <input type="text" class="form-control" id="profile_location" name="location">
-            </div> -->
+              <label for="suburb">Suburb</label>
+              <input id="suburb" type="text" class="form-control" name="suburb" required>
+            </div>
+            <div class="form-group">
+              <label for="postcode">Postcode</label>
+              <input id="postcode" type="text" class="form-control" name="postcode" required>
+            </div>
+            <div class="form-group">
+              <label for="state">State</label>
+              <select class="" required>
+                <option>ACT</option>
+                <option>NSW</option>
+                <option>NT</option>
+                <option>QLD</option>
+                <option>TAS</option>
+                <option>VIC</option>
+                <option>WA</option>
+              </select>
+            </div>
 
             <div class="form-group">
               <label for="profile_email">Email</label>
               <input type="text" class="form-control" id="profile_email" name="email" value="<?php echo $email; ?>">
             </div>
 
-            <!-- <div class="form-group">
+            <div class="form-group">
+              <label for="phone">Phone</label>
+              <input id="phone" type="text" class="form-control" name="phone" required>
+            </div>
+
+            <div class="form-group">
               <label for="profile_dob">Birthday</label>
               <input type="text" class="form-control" id="profile_dob" name="dob">
-            </div> -->
+            </div>
 
-            <!-- <label> On Zootopia I want to:</label>
+            <label> On Zootopia I want to:</label>
             <div class="form-check">
               <label class="form-check-label">
                 <input class="form-check-input" type="checkbox" value="" name="make_bookings">
@@ -84,9 +105,18 @@ $email = $updateProfile['loginUsername'];
                 <input class="form-check-input" type="checkbox" value="" name="post_ads">
                 Earn money (post pet ads)
               </label>
-            </div> -->
+            </div>
 
-            <input type="button" value="Update" onclick="showMsg(<?php echo $login_ID; ?>)"/>
+            <input type="button" value="Update" onclick="showMsg(<?php echo $login_ID;?>)"/>
+
+            <?php
+
+            if(!isset($_SESSION['message'])){
+              $_SESSION['message'] = "";
+            }
+            echo $_SESSION['message'];
+            unset ($_SESSION['message']);
+            ?>
 
             <div id="message"></div>
           <!-- </div> -->
@@ -95,3 +125,61 @@ $email = $updateProfile['loginUsername'];
 
   </div>
 </body>
+
+
+<!--************************** SECOND PART OF REGISTRATION **************************-->
+							<!-- <form id="register_section2" action="../php/submit_register2.php" method="POST">
+
+								<div class="form-group">
+									<label for="user_type">Account Type</label>
+									<select class="">
+					          <option name"user_type">I want to be a Customer to make bookings</option>
+					          <option name"user_type">I want to be a Pet Owner to post ads</option>
+									</select>
+								</div>
+
+								<div class="form-group">
+									<label for="phone">Phone</label>
+									<input id="phone" type="text" class="form-control" name="phone" required>
+								</div> -->
+
+								<!-- <div class="form-group no-margin">
+									<button type="submit" class="btn btn-primary btn-block">
+										Next
+									</button>
+								</div> -->
+							<!-- </form> -->
+<!--************************** SECOND PART OF REGISTRATION **************************-->
+
+<!--************************** THIRD PART OF REGISTRATION **************************-->
+							<!-- <form action="../php/submit_register2.php" method="POST"> -->
+
+
+								<!-- <div class="form-group">
+									<label for="suburb">Suburb</label>
+									<input id="suburb" type="text" class="form-control" name="suburb" required>
+								</div>
+								<div class="form-group">
+									<label for="postcode">Postcode</label>
+									<input id="postcode" type="text" class="form-control" name="postcode" required>
+								</div>
+								<div class="form-group">
+									<label for="state">State</label>
+									<select class="" required>
+										<option>ACT</option>
+										<option>NSW</option>
+										<option>NT</option>
+										<option>QLD</option>
+										<option>TAS</option>
+										<option>VIC</option>
+										<option>WA</option>
+									</select>
+								</div>
+
+								<div class="form-group no-margin">
+									<button type="submit" class="btn btn-primary btn-block">
+										Finish
+									</button>
+								</div>
+							</form> -->
+<!--************************** THIRD PART OF REGISTRATION **************************-->
