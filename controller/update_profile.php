04fc5a87ecd9bbe5d910e39f44a9da159bb3e028
JessicaYeoh <30559012+JessicaYeoh@users.noreply.github.com>
2018-03-27 12:19:49 +1000

    ensured updateUser() returns object to ajax

--- controller/update_profile.php
+++ controller/update_profile.php
@@ -4,31 +4,36 @@ header('Content-Type: application/json');
 include '../model/db.php';
 $conn = connect();
 
-$login_ID = $_GET['loginID'];
 
-if(isset($login_ID)) {
+if(isset($_GET['loginID']) && empty($_POST)) {
+  $login_ID = $_GET['loginID'];
+
   // if($_GET['loginID'] == $_SESSION['userID']) {
     $result = getOneUser($login_ID);
     if(is_array($result)) {
       echo json_encode($result);
+      // echo $result;
     } else {
       echo json_encode(Array('userdata'=>false));
+        // echo "false";
     }
-  // }
+    exit();
 }
 
-if(isset($_POST)) {
+if(isset($_GET['loginID']) && !empty($_POST)) {
+  $login_ID = $_GET['loginID'];
 
   if(updateUser($_POST, $login_ID)) {
-   //  echo json_encode(Array('userUpdate'=>true));
-      return true;
+    echo json_encode(Array('userUpdate'=>true));
+      // $_SESSION['message'] = 'done';
+      //  echo "updated";
+      //  return true;
   } else {
-   //  echo json_encode(Array('userUpdate'=>false));
-      return false;
+    echo json_encode(Array('userUpdate'=>false));
+      // echo "error";
+      // return false;
   }
   exit();
 }
-
-
 
 ?>
